96bf6508ee251977724e7d5f0ddc20aac52d1f49
LeThanhCong299 <64365221+LeThanhCong299@users.noreply.github.com>
2021-03-03 16:00:41 +0700

    ModuleTest

---
+++ IModule_Service.cs
@@ -0,0 +1,25 @@
+﻿using Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALPSService
+{
+    [ServiceContract]
+    public interface IModule_Service
+    {
+        [OperationContract]
+        List<ModuleDTO> findAllModule();
+        [OperationContract]
+        ModuleDTO findModuleById(string ModuleID);
+        [OperationContract]
+        void createModule(ModuleDTO newModule);
+        [OperationContract]
+        void updateModule(ModuleDTO updateModule);
+        [OperationContract]
+        void deleteModule(string ModuleID);
+    }
+}
